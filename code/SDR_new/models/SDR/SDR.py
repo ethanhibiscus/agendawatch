f0c9b25a1f2657777c22c7820028a1ec7b9a4b79
hibiscusethan <ethanhsu@stanford.edu>
2024-07-01 16:58:16 -0700

    moo

--- code/SDR_new/models/SDR/SDR.py
+++ code/SDR_new/models/SDR/SDR.py
@@ -145,33 +145,17 @@ class SDR(TransformersBase):
 
     def dataloader(self, mode=None):
         if mode == "train":
-            sampler = MPerClassSampler(
-                self.train_dataset.labels,
-                2,
-                batch_size=self.hparams.train_batch_size,
-                length_before_new_iter=(self.hparams.limit_train_batches) * self.hparams.train_batch_size,
-            )
-
             loader = DataLoader(
                 self.train_dataset,
                 num_workers=self.hparams.num_data_workers,
-                sampler=sampler,
                 batch_size=self.hparams.train_batch_size,
                 collate_fn=partial(reco_sentence_collate, tokenizer=self.tokenizer,),
             )
 
         elif mode == "val":
-            sampler = MPerClassSamplerDeter(
-                self.val_dataset.labels,
-                2,
-                length_before_new_iter=self.hparams.limit_val_indices_batches,
-                batch_size=self.hparams.val_batch_size,
-            )
-
             loader = DataLoader(
                 self.val_dataset,
                 num_workers=self.hparams.num_data_workers,
-                sampler=sampler,
                 batch_size=self.hparams.val_batch_size,
                 collate_fn=partial(reco_sentence_collate, tokenizer=self.tokenizer,),
             )
