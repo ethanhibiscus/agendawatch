7f958f46d7115d1956cb5323f9a821b8951e19d5
hibiscusethan <ethanhsu@stanford.edu>
2024-07-18 14:39:51 -0700

    moo

--- recommendation_systems/SDR/sdr_inference.py
+++ recommendation_systems/SDR/sdr_inference.py
@@ -1,21 +1,25 @@
 import os
 import torch
 from transformers import RobertaTokenizer
-from models.SDR.similarity_modeling import SimilarityModeling
+from pytorch_lightning import Trainer
+from models.SDR.SDR import SDR
 from data.datasets import CustomTextDatasetParagraphsSentencesTest
 from sklearn.metrics.pairwise import cosine_similarity
 from torch.utils.data import DataLoader
 
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 # Load the model checkpoint
-checkpoint_path = '~/03_07_2024-23_10_34/epoch=10.ckpt'
-model = SimilarityModeling.load_from_checkpoint(checkpoint_path)
+checkpoint_path = os.path.expanduser('~/03_07_2024-23_10_34/epoch=10.ckpt')
+model = SDR.load_from_checkpoint(checkpoint_path).to(device)
 
 # Initialize the tokenizer
 tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
 
 # Load source document and target documents
 source_document_path = './data/text_files/source.txt'
-target_documents_dir = './data/text_files/targets/'
+target_documents_dir = './data/text_files/'
 
 # Read source document
 with open(source_document_path, 'r', encoding='utf-8') as file:
@@ -29,19 +33,19 @@ for filename in os.listdir(target_documents_dir):
             target_texts.append((filename, file.read()))
 
 # Preprocess documents using the CustomTextDatasetParagraphsSentencesTest
-source_dataset = CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.max_len, mode='test')
-target_datasets = [CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.max_len, mode='test') for _, text in target_texts]
+source_dataset = CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.model_max_length, mode='test')
+target_datasets = [CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.model_max_length, mode='test') for _, text in target_texts]
 
 # Create DataLoader for source and target documents
 source_loader = DataLoader(source_dataset, batch_size=1, shuffle=False)
 target_loaders = [DataLoader(target_dataset, batch_size=1, shuffle=False) for target_dataset in target_datasets]
 
 # Get embeddings for source document
-source_embeddings = []
 model.eval()
+source_embeddings = []
 with torch.no_grad():
     for batch in source_loader:
-        inputs = batch[0].to(model.device)
+        inputs = batch[0].to(device)
         outputs = model(inputs)
         source_embeddings.append(outputs[2].cpu())
 
@@ -51,7 +55,7 @@ for target_loader in target_loaders:
     target_embedding = []
     with torch.no_grad():
         for batch in target_loader:
-            inputs = batch[0].to(model.device)
+            inputs = batch[0].to(device)
             outputs = model(inputs)
             target_embedding.append(outputs[2].cpu())
     target_embeddings.append(torch.cat(target_embedding, dim=0))
--- recommendation_systems/SDR/sdr_main.py
+++ recommendation_systems/SDR/sdr_main.py
@@ -1,81 +1,79 @@
-"""Top level file, parse flags and call trining loop."""
 import os
-from utils.pytorch_lightning_utils.pytorch_lightning_utils import load_params_from_checkpoint
 import torch
-from pytorch_lightning.profiler.profilers import SimpleProfiler
-from utils.pytorch_lightning_utils.callbacks import RunValidationOnStart
-from utils import switch_functions
-import pytorch_lightning
-from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
-from utils.argparse_init import default_arg_parser, init_parse_argparse_default_params
-import logging
-logging.basicConfig(level=logging.INFO)
-from pytorch_lightning.loggers import TensorBoardLogger
+from transformers import RobertaTokenizer
+from models.SDR.SDR import SDR
+from data.datasets import CustomTextDatasetParagraphsSentencesTest
+from sklearn.metrics.pairwise import cosine_similarity
+from torch.utils.data import DataLoader
 
-def main():
-    """Initialize all the parsers, before training init."""
-    parser = default_arg_parser()
-    parser = Trainer.add_argparse_args(parser)  # Bug in PL
-    parser = default_arg_parser(description="docBert", parents=[parser])
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    eager_flags = init_parse_argparse_default_params(parser)
-    model_class_pointer = switch_functions.model_class_pointer(eager_flags["task_name"], eager_flags["architecture"])
-    parser = model_class_pointer.add_model_specific_args(parser, eager_flags["task_name"], eager_flags["dataset_name"])
+# Load the model checkpoint
+checkpoint_path = '~/03_07_2024-23_10_34/epoch=10.ckpt'
+model = SDR.load_from_checkpoint(checkpoint_path).to(device)
 
-    hyperparams = parser.parse_args()
-    main_train(model_class_pointer, hyperparams,parser)
+# Initialize the tokenizer
+tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
 
+# Load source document and target documents
+source_document_path = './data/text_files/ab-bonnyville_civicplus_06092020-270.txt'
+target_documents_dir = './data/text_files/'
 
-def main_train(model_class_pointer, hparams, parser):
-    """Initialize the model, call training loop."""
-    pytorch_lightning.utilities.seed.seed_everything(seed=hparams.seed)
+# Read source document
+with open(source_document_path, 'r', encoding='utf-8') as file:
+    source_text = file.read()
 
-    if(hparams.resume_from_checkpoint not in [None,'']):
-        hparams = load_params_from_checkpoint(hparams, parser)
+# Read target documents
+target_texts = []
+for filename in os.listdir(target_documents_dir):
+    if filename.endswith('.txt') and filename != os.path.basename(source_document_path):
+        with open(os.path.join(target_documents_dir, filename), 'r', encoding='utf-8') as file:
+            target_texts.append((filename, file.read()))
 
-    model = model_class_pointer(hparams)
+# Preprocess documents using the CustomTextDatasetParagraphsSentencesTest
+source_dataset = CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.model_max_length, mode='test')
+target_datasets = [CustomTextDatasetParagraphsSentencesTest(tokenizer, None, 'custom_dataset', tokenizer.model_max_length, mode='test') for _, text in target_texts]
 
+# Create DataLoader for source and target documents
+source_loader = DataLoader(source_dataset, batch_size=1, shuffle=False)
+target_loaders = [DataLoader(target_dataset, batch_size=1, shuffle=False) for target_dataset in target_datasets]
 
-    logger = TensorBoardLogger(save_dir=model.hparams.hparams_dir,name='',default_hp_metric=False)
-    logger.log_hyperparams(model.hparams, metrics={model.hparams.metric_to_track: 0})
-    print(f"\nLog directory:\n{model.hparams.hparams_dir}\n")
+# Get embeddings for source document
+model.eval()
+source_embeddings = []
+with torch.no_grad():
+    for batch in source_loader:
+        inputs = batch[0].to(device)
+        outputs = model(inputs)
+        source_embeddings.append(outputs[2].cpu())
 
-    trainer = pytorch_lightning.Trainer(
-        num_sanity_val_steps=2,
-        gradient_clip_val=hparams.max_grad_norm,
-        callbacks=[RunValidationOnStart()],
-        checkpoint_callback=ModelCheckpoint(
-            save_top_k=3,
-            save_last=True,
-            mode="min" if "acc" not in hparams.metric_to_track else "max",
-            monitor=hparams.metric_to_track,
-            filepath=os.path.join(model.hparams.hparams_dir, "{epoch}"),
-            verbose=True,
-        ),
-        logger=logger,
-        #precision=16,
-        max_epochs=hparams.max_epochs,
-        gpus=hparams.gpus,
-        distributed_backend="dp",
-        limit_val_batches=hparams.limit_val_batches,
-        limit_train_batches=hparams.limit_train_batches,
-        limit_test_batches=hparams.limit_test_batches,
-        check_val_every_n_epoch=hparams.check_val_every_n_epoch,
-        profiler=SimpleProfiler(),
-        accumulate_grad_batches=hparams.accumulate_grad_batches,
-        reload_dataloaders_every_epoch=True,
-        # load
-        resume_from_checkpoint=hparams.resume_from_checkpoint,
-    )
-    if(not hparams.test_only):
-        trainer.fit(model)
-    else:
-        if(hparams.resume_from_checkpoint is not None):
-            model = model.load_from_checkpoint(hparams.resume_from_checkpoint,hparams=hparams, map_location=torch.device(f"cpu"))
-    trainer.test(model)
+# Get embeddings for target documents
+target_embeddings = []
+for target_loader in target_loaders:
+    target_embedding = []
+    with torch.no_grad():
+        for batch in target_loader:
+            inputs = batch[0].to(device)
+            outputs = model(inputs)
+            target_embedding.append(outputs[2].cpu())
+    target_embeddings.append(torch.cat(target_embedding, dim=0))
 
+# Compute cosine similarity
+# Flatten source embeddings
+source_embedding = torch.cat(source_embeddings, dim=0).mean(dim=0).numpy()
 
-if __name__ == "__main__":
-    main()
+# Compute similarity scores for each target document
+similarity_scores = []
+for target_embedding in target_embeddings:
+    target_embedding_mean = target_embedding.mean(dim=0).numpy()
+    score = cosine_similarity([source_embedding], [target_embedding_mean])[0][0]
+    similarity_scores.append(score)
 
+# Rank target documents based on similarity scores
+ranked_targets = sorted(zip(target_texts, similarity_scores), key=lambda x: x[1], reverse=True)
+
+# Display ranked documents
+print("Ranked target documents based on similarity to source document:")
+for (filename, _), score in ranked_targets:
+    print(f"Document: {filename}, Similarity Score: {score}")
